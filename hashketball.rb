def73d7d8d9bbd9e13c04dd0d918a0f95ac32f78
Matthew Bartl <mattbartl@gmail.com>
2019-03-27 02:24:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -75,7 +75,8 @@ end
 #Returns a specific player faster
 def find_a_player(player)
   game_hash.map do |location, team_data|
-    team_data[:players].last.select {|name| name.to_s == player}
+    output = team_data[:players].select {|name| name.to_s == player}
+    return output.last
   end
 end
 find_a_player('Brendan Haywood')
