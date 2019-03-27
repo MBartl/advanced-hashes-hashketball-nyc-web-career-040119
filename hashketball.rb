ab027672ad0b6c229cac780c1958f011eb0da0a4
Matthew Bartl <mattbartl@gmail.com>
2019-03-27 01:58:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -75,7 +75,7 @@ end
 #Returns a specific player
 def find_a_player(player)
   game_hash.map do |location, team_data|
-    return team_data[:players]
+    return team_data
     team_data[:players].select {|name| name.to_s == player}
   end
 end
