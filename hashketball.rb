c7fbcda33132ca634acba8aa5143fffc5eac1163
Matthew Bartl <mattbartl@gmail.com>
2019-03-27 02:51:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -86,7 +86,7 @@ end
 
 #Method No. 1: Number of Points Scored by Player
 def num_points_scored(player)
-  find_player(player) {|name, stats| stats[:points]|
+  find_player(player) {|name, stats| stats[:points]|}
 end
 num_points_scored("Brendan Haywood")
 
